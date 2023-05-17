8c12304f393b1b3144bd3bb5e88227a26d342f39
Shemeer <shameerbem@gmail.com>
2023-05-17 22:00:09 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [text,setText] = useState('');
+  function myFunc(e){
+    setText(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <label>Enter your name:</label>
+       <input onChange={myFunc} type="text"/>
+       {text?<p>Hello {text}!</p>:null}
     </div>
   )
 }
